26158421a71baca06d666ea862c7943f5e184ff4
Jon Ng <jonnynch@gmail.com>
2023-05-01 11:41:53 +1000

    update all

---
+++ marathon/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+dockerd  &
+/opt/zookeeper/bin/zkServer.sh start
+# mesos-master --work_dir=/var/lib/mesos --advertise_ip=127.0.0.1 --no-hostname_lookup --ip=127.0.0.1  > /var/log/mesos-master.log 2>&1 &
+mesos-master --work_dir=/var/lib/mesos --advertise_ip=127.0.0.1 --hostname=mesos-master.localdev.me  > /var/log/mesos-master.log 2>&1 &
+# mesos-slave --work_dir=/var/lib/mesos --master=localhost:5050 --containerizers=docker,mesos --no-hostname_lookup --ip=127.0.0.1 > /var/log/mesos-slave.log 2>&1 &
+mesos-slave --work_dir=/var/lib/mesos --master=mesos-master.localdev.me:5050 --containerizers=docker,mesos --hostname=mesos-slave.localdev.me > /var/log/mesos-slave.log 2>&1 &
+/usr/share/marathon/bin/marathon --master=mesos-master.localdev.me:5050 --disable_ha > /var/log/marathon.log 2>&1 
